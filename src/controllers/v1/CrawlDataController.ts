497652ee1ddf2a11a89479b09709439f8cde61f6
jenybo <86452879+JenyBo@users.noreply.github.com>
2023-10-16 21:51:00 +0700

    feature:Init CrawlDataController

---
+++ src/controllers/v1/CrawlDataController.ts
@@ -0,0 +1,47 @@
+import {
+  Route,
+  Controller,
+  Tags,
+  OperationId,
+  TsoaResponse,
+  Res,
+  Query,
+  Get,
+} from 'tsoa';
+import { injectable } from 'inversify';
+
+import { lazyInject } from '@n-configs/container';
+import { SERVICES } from '@n-types/injections/services';
+import { ITikTokServices } from '@n-services/interface/ITikTokServices';
+import { InternalError } from '@n-errors/HttpError';
+import { CResponse } from '../types';
+
+@injectable()
+@Tags('Crawl')
+@Route('api/v1/crawl')
+export class CrawlDataController extends Controller {
+  @lazyInject(SERVICES.TikTokServices)
+  private tiktokServices: ITikTokServices;
+
+  @Get('/tiktok')
+  @OperationId('crawlTiktokData')
+  public async crawlTiktokData(
+    @Res() res: TsoaResponse<200, CResponse>,
+    @Query() post_id: string,
+    @Query() region?: string,
+  ): Promise<CResponse> {
+    try {
+      const post = await this.tiktokServices.getPostById(post_id, region);
+      return {
+        message: `Crawl tiktok post: ${post_id} successfully`,
+        status: 'success',
+        data: post,
+      };
+    } catch (error: any) {
+      return res(error.code || 500, {
+        message: error.message,
+        status: 'error',
+      });
+    }
+  }
+}
